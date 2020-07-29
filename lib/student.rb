faa398f8b8bd858508f5095cf0481fde56139925
Yehong Piao <piaoyehong1107@gmail.com>
2020-07-29 21:01:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  attr_reader :knowledge
+
+def initialize (knowledge=[])
+  @knowledge=knowledge
+end
+
+def learn (string)
+  @knowledge.push(string)
+end
 
 end
